4fe05088d0ee57ea99b37721f1cdc2e2c91c8c59
stanislas jouven <stanislasjouven@gmail.com>
2019-09-28 16:08:19 +0200

    Firt version of the TRBS algorithm

---
+++ source_estimation.py
@@ -0,0 +1,24 @@
+import networkx as nx
+import numpy as np
+
+
+def ml_estimate(graph, obs_time, paths, path_lengths):
+
+    T = {}
+    var_T = {}
+
+    for node in list(graph.nodes()):
+        T.setdefault(node, [])
+        for obs in np.array(list(obs_time.keys())):
+            T[node].append(obs_time[obs] - path_lengths[obs][node])
+        var_T[node] = np.var(T[node])
+
+
+    min_var = np.min(var_T.values())
+    source_candidates = list()
+        ### Finds nodes with maximum likelihood
+        for src, value in posterior.items():
+            if np.isclose(value, min_var, atol= 1e-08):
+                source_candidates.append(src)
+
+    return source_candidates, var_T
---
+++ source_loc.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def trbs(graph, obs_time, distribution):
+
+    path_lengths = {}
+    paths = {}
+    for o in obs:
+        path_lengths[o], paths[o] = nx.single_source_dijkstra(graph, o)
+
+    ### Run the estimation
+    s_est, likelihoods = se.ml_estimate(graph, obs_time, paths, path_lengths)
+
+    ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=True)
+
+    return (s_est, ranked)
