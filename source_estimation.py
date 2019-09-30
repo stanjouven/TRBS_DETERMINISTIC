edcefd4b75c54a53f775e2ce05d07d44bec35360
stanislas jouven <stanislasjouven@gmail.com>
2019-09-30 22:52:21 +0200

    testing with code from before

--- source_estimation.py
+++ source_estimation.py
@@ -2,6 +2,27 @@ import networkx as nx
 import numpy as np
 
 
+def source_estimate(graph, obs_time, paths, path_lengths):
+
+    T = {}
+    var_T = {}
+
+    for node in list(graph.nodes()):
+        T.setdefault(node, [])
+        for obs in np.array(list(obs_time.keys())):
+            T[node].append(obs_time[obs] - path_lengths[obs][node])
+        var_T[node] = np.var(T[node])
+
+    min_var = np.min(list(var_T.values()))
+    source_candidates = list()
+    ### Finds nodes with maximum likelihood
+    for src, value in var_T.items():
+        if np.isclose(value, min_var, atol= 1e-08):
+            source_candidates.append(src)
+
+    return source_candidates, var_T
+
+'''
 def source_estimate(graph, obs_time, path_lengths):
 
     T = {}
@@ -22,3 +43,4 @@ def source_estimate(graph, obs_time, path_lengths):
             source_candidates.append(src)
 
     return source_candidates, var_T
+'''
--- source_loc.py
+++ source_loc.py
@@ -8,6 +8,24 @@ def trbs(graph, obs_time, distribution):
 
     path_lengths = {}
     paths = {}
+
+    obs = np.array(list(obs_time.keys()))
+    for o in obs:
+        path_lengths[o], paths[o] = nx.single_source_dijkstra(graph, o)
+
+    ### Run the estimation
+    s_est, likelihoods = se.source_estimate(graph, obs_time, paths, path_lengths)
+
+    ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=True)
+    
+
+    return (s_est, ranked)
+
+'''
+def trbs(graph, obs_time, distribution):
+
+    path_lengths = {}
+    paths = {}
     iso_nodes = []
     isolates = nx.isolates(graph)
     for node in list(graph.nodes()):
@@ -33,7 +51,7 @@ def trbs(graph, obs_time, distribution):
     ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=True)
 
     return (s_est, ranked)
-
+'''
 '''
 def trbs(graph, obs_time, distribution):
 
