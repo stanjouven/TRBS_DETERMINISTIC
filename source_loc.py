4789b3f7fa7bbe5aee50869e93a0ca0d5aa689b2
stanislas jouven <stanislasjouven@gmail.com>
2019-10-04 16:34:02 +0200

    Because the graph can have multiple component: we have to find the largest one and check if all the observers are in it

--- source_loc.py
+++ source_loc.py
@@ -4,6 +4,7 @@ import operator
 
 import TRBS.source_estimation as se
 
+'''
 def trbs(graph, obs_time, distribution):
 
     path_lengths = {}
@@ -38,22 +39,32 @@ def trbs(graph, obs_time, distribution):
     ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=True)
 
     return (s_est, ranked)
-
 '''
+
 def trbs(graph, obs_time, distribution):
 
+    largest_graph_cc = max(nx.connected_components(G), key=len)
+    obs_time_filt = observer_filtering(obs_time, largest_graph_cc)
+    obs_filt = np.array(list(obs_time.keys()))
     path_lengths = {}
-    obs = np.array(list(obs_time.keys()))
-    for o in obs:
-        path_lengths[o] = preprocess(o, graph, distribution)
+    print("graph component", list(largest_graph_cc.nodes()))
+    print("obs time", list(obs_time.keys()))
+    print("obs time filt", list(obs_filt.keys()))
+
+    for o in obs_filt:
+        path_lengths[o] = preprocess(o, largest_graph_cc, distribution)
     ### Run the estimation
-    s_est, likelihoods = se.source_estimate(graph, obs_time, path_lengths)
+    s_est, likelihoods = se.source_estimate(largest_graph_cc, obs_time_filt, path_lengths)
 
     ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=True)
     print('ranked', ranked)
 
     return (s_est, ranked)
 
+'''
+Apply the given distribution to the edge of the graph.
+Return dictionnary: node -> time to go from that node to the given observer
+'''
 def preprocess(observer, graph, distr):
     ### Initialization of the edge delay
     edges = graph.edges()
@@ -62,4 +73,16 @@ def preprocess(observer, graph, distr):
 
     ### Computation of the shortest paths from every observer to all other nodes
     return  nx.single_source_dijkstra_path_length(graph, observer)
+
 '''
+ Check if every observer is part of the largest graph component
+ OBS_TIME: dictionnary: node -> time of infection
+ LARGEST_GRAPH_CC: largest component of the graph
+ RETURN the obs_time dictionnary without the ones that are not part of largest_graph_cc
+'''
+def observer_filtering(obs_time, largest_graph_cc):
+    obs = np.array(list(obs_time.keys()))
+    for o in obs:
+        if !largest_graph_cc.has_node(o):
+            obs_time.delete(o)
+    return obs_time
