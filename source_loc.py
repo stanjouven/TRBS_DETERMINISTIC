b2a9b06b6dccc39a0306adb0d1283af1a17e1190
stanislas jouven <stanislasjouven@gmail.com>
2019-10-05 16:04:38 +0200

    As the source is expected to be the lower variance we sort in increasing order

--- source_loc.py
+++ source_loc.py
@@ -57,7 +57,7 @@ def trbs(graph, obs_time, distribution):
     ### Run the estimation
     s_est, likelihoods = se.source_estimate(largest_graph_cc, obs_time_filt, path_lengths)
 
-    ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=True)
+    ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=False)
     #print('ranked', ranked)
 
     return (s_est, ranked)
