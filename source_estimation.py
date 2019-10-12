719647f319aba2137c11d09241d96d8507ec73e2
stanislas jouven <stanislasjouven@gmail.com>
2019-10-12 17:27:56 +0200

    deleted prints

--- source_estimation.py
+++ source_estimation.py
@@ -10,9 +10,9 @@ def source_estimate(graph, obs_time, path_lengths):
         T.setdefault(node, [])
         for obs in np.array(list(obs_time.keys())):
             T[node].append(obs_time[obs] - path_lengths[obs][node])
-            print('T[', node, '] = ', T[node])
+            #print('T[', node, '] = ', T[node])
         var_T[node] = np.var(T[node])
-        print('var T = ', var_T[node])
+        #print('var T = ', var_T[node])
 
     min_var = np.min(list(var_T.values()))
     source_candidates = list()
@@ -20,5 +20,5 @@ def source_estimate(graph, obs_time, path_lengths):
     for src, value in var_T.items():
         if np.isclose(value, min_var, atol= 1e-08):
             source_candidates.append(src)
-    print('source_candidates = ', source_candidates)
+    #print('source_candidates = ', source_candidates)
     return source_candidates, var_T
--- source_loc.py
+++ source_loc.py
@@ -13,7 +13,7 @@ PARAMETERS:
 '''
 def trbs(graph, obs_time_filt, distribution):
 
-    print('obs time', obs_time_filt)
+    #print('obs time', obs_time_filt)
 
     #largest_graph_cc = graph.subgraph(max(nx.connected_components(graph), key=len))
     #obs_time_filt = observer_filtering(obs_time, largest_graph_cc)
@@ -22,11 +22,11 @@ def trbs(graph, obs_time_filt, distribution):
 
     for o in obs_filt:
         path_lengths[o] = preprocess(o, graph, distribution)
-        print('path_lengths', o, '= ', path_lengths[o])
+        #print('path_lengths', o, '= ', path_lengths[o])
     ### Run the estimation
     s_est, likelihoods = se.source_estimate(graph, obs_time_filt, path_lengths)
     ranked = sorted(likelihoods.items(), key=operator.itemgetter(1), reverse=False)
-    print('ranked', ranked)
+    #print('ranked', ranked)
 
     return (s_est, ranked)
 
@@ -43,7 +43,7 @@ def preprocess(observer, graph, distr):
     edges = graph.edges()
     for (u, v) in edges:
         graph[u][v]['weight'] = abs(distr.rvs())
-        print('u: ',u, 'v: ', v, graph[u][v]['weight'])
+        #print('u: ',u, 'v: ', v, graph[u][v]['weight'])
 
     ### Computation of the shortest paths from every observer to all other nodes
     return  nx.single_source_dijkstra_path_length(graph, observer)
