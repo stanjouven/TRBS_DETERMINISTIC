2afaa80f52fbdfd70c876c37da74f9b4ded66b90
stanislas jouven <stanislasjouven@gmail.com>
2019-10-24 18:36:51 +0200

    print

--- source_estimation.py
+++ source_estimation.py
@@ -3,13 +3,14 @@ import numpy as np
 
 
 def source_estimate(graph, obs_time, path_lengths):
-    #print('path length ', path_lengths)
+    print('path length ', path_lengths)
     #print('obs ', list(obs_time.keys()))
     T = {}
     var_T = {}
     for node in list(graph.nodes()):
         T.setdefault(node, [])
         for obs in np.array(list(obs_time.keys())):
+            print('obs ', obs)
             a = path_lengths[obs][node]
             T[node].append(obs_time[obs] - path_lengths[obs][node])
             #print('T[', node, '] = ', T[node])
